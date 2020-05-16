d6ae79d6f4d1e0bcc0d3648106980cfe8bf23b1d
Quellens <57103965+Quellens@users.noreply.github.com>
2020-05-16 14:03:48 +0200

    Add files via upload

---
+++ Riggler Scud/sounds.js
@@ -0,0 +1,71 @@
+var easy = new Audio();
+easy.src = "ja%20easy.mp3";
+var elem1 = document.getElementById("easy")
+elem1.addEventListener("mouseover", ()=>{
+    easy.play();
+    neutral.pause();
+    heftich.pause();
+    controls.pause();
+})
+
+var neutral = new Audio();
+neutral.src = "neutral.mp3";
+var elem2 = document.getElementById("neutral")
+elem2.addEventListener("mouseover", ()=>{
+    neutral.play();
+    easy.pause();
+    heftich.pause();
+    controls.pause();
+})
+
+var heftich = new Audio();
+heftich.src = "heftig%20ey.mp3";
+var elem3 = document.getElementById("heftich")
+elem3.addEventListener("mouseover", ()=>{
+    heftich.play();
+    easy.pause();
+    neutral.pause();
+    controls.pause();
+    
+})
+
+var controls = new Audio();
+controls.src = "controls.mp3";
+var elem4 = document.getElementById("controls")
+elem4.addEventListener("mouseover", ()=>{
+    controls.play();
+    heftich.pause();
+    neutral.pause();
+    easy.pause();
+})
+
+
+var riggler = document.getElementById("riggler");
+riggler.volume= 0.5;
+
+var i=0;
+var mutebutton = document.getElementById("mute")
+mutebutton.addEventListener("click",()=>{
+    i++;
+    if(i%2 == 1){
+    riggler.volume = 0;
+    mutebutton.innerHTML = "Stummschaltung aufheben"; 
+    heftich.pause();
+    neutral.pause();
+    easy.pause();
+    controls.pause();
+    heftich.volume = 0;
+    neutral.volume = 0;
+    easy.volume =0;
+    controls.volume = 0;
+    } else{
+    riggler.volume = 0.5;
+    mutebutton.innerHTML = "Stummschalten";
+    heftich.volume = 1;
+    neutral.volume = 1;
+    easy.volume =1;
+    controls.volume = 1;
+    
+    }
+   
+})
